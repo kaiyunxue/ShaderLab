e06b592dd4b9fce5304d20187b98dac08cd6757e
KevinXue <kevinxue0239@gmail.com>
2019-01-22 13:32:14 -0500

    update

---
+++ Assets/Scripts/RotatingController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingController : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        transform.Rotate(transform.up, speed);
+    }
+}
---
+++ Assets/Scripts/ShaderController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderController : MonoBehaviour
+{
+    public Material material;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).GetComponent<Renderer>().material = material;
+        }
+    }
+    private void Update()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).GetComponent<RotatingController>().speed = speed;
+        }
+    }
+}
