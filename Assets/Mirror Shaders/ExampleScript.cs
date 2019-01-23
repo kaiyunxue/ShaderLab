d174eec4bca65b853c2c9b4ce394bec3f8dc2e03
KevinXue <kevinxue0239@gmail.com>
2019-01-22 22:15:32 -0500

    Mirror

---
+++ Assets/Mirror Shaders/ExampleScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ExampleScript : MonoBehaviour
+{
+    public Renderer rend;
+
+    private float timePass = 0.0f;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        Debug.Log("aaa");
+    }
+
+    void OnWillRenderObject()
+    {
+        timePass += Time.deltaTime;
+
+        if (timePass > 1.0f)
+        {
+            timePass = 0.0f;
+            Debug.Log(gameObject.name + " is being rendered by " + Camera.current.name + " at " + Time.time);
+        }
+    }
+}
